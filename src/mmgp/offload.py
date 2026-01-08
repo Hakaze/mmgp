54340bdf0f5240145a5fd5961ca9d42d092511f1
Tommy Cresine <tcresine@gmail.com>
2026-01-08 09:06:21 -0500

    disable torch compile for inductor graph break warning in wan2gp

--- src/mmgp/offload.py
+++ src/mmgp/offload.py
@@ -2804,6 +2804,7 @@ class offload:
 
 
 
+    @torch.compiler.disable()
     def _lora_linear_forward(self, model, submodule, loras_data, x: torch.Tensor, *args, **kwargs) -> torch.Tensor:
         weight = submodule.weight
         bias = submodule.bias
